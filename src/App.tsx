707425a6e602f2fd1d2269980cdfd4fd1033058d
Ololade <73138104+zololade@users.noreply.github.com>
2026-08-28 21:16:16 +0100

    refactor: implement Input component and update App to use controlled input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { useState, type ChangeEvent, type ChangeEventHandler } from "react";
+
+function Input({
+  handleChange,
+  inputValue,
+}: {
+  handleChange: ChangeEventHandler;
+  inputValue: string;
+}) {
+  return <input type="text" onChange={handleChange} value={inputValue} />;
+}
+
 function App() {
+  const [inputValue, setInputValue] = useState("");
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setInputValue(e.target.value);
+  }
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>{inputValue}</h1>
+      <Input handleChange={handleChange} inputValue={inputValue} />
     </div>
   );
 }
-
-export { App };
+export { App, Input };
--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
 import { type ReactNode } from "react";
 
-import { App } from "../src/App";
+import { App, Input } from "../src/App";
 
 function setup(jsx: ReactNode) {
   return {
@@ -12,8 +12,21 @@ function setup(jsx: ReactNode) {
 }
 
 describe("App", () => {
-  test("render h1 element", () => {
-    setup(<App />);
-    expect(screen.getByRole("heading", { name: /hello world/i, level: 1 })).toBeInTheDocument();
+  test("input gets value", async () => {
+    const { user } = setup(<App />);
+    const input: HTMLInputElement = screen.getByRole("textbox");
+
+    await user.type(input, "React");
+
+    expect(input.value).toBe("React");
+  });
+
+  test("if input was changed", async () => {
+    let mock = vi.fn<() => void>();
+    const { user } = setup(<Input handleChange={mock} inputValue="" />);
+    const input: HTMLInputElement = screen.getByRole("textbox");
+
+    await user.type(input, "React");
+    expect(mock).toHaveBeenCalledTimes(5);
   });
 });
